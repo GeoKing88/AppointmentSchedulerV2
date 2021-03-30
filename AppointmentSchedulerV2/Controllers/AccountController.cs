33d17f616b4069f61745d05c1b95048531f0a6f2
Henrique Primo <henrique.primo@contisystems.pt>
2021-03-30 20:52:23 +0100

    Add project files.

---
+++ AppointmentSchedulerV2/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppointmentSchedulerV2.Models;
+using AppointmentSchedulerV2.Models.ApplicationDbContext;
+using AppointmentSchedulerV2.Models.ViewModels;
+using AppointmentSchedulerV2.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppointmentScheduling.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        UserManager<ApplicationUser> _userManager;
+        SignInManager<ApplicationUser> _signInManager;
+        RoleManager<IdentityRole> _roleManager;
+
+        public AccountController(ApplicationDbContext db, UserManager<ApplicationUser> userApplication, SignInManager<ApplicationUser> signInManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userApplication;
+            _signInManager = signInManager;
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            if (!_roleManager.RoleExistsAsync(Helper.Admin).GetAwaiter().GetResult())
+            {
+                //var teste = new IdentityRole("Tes");
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Admin));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Doctor));
+                await _roleManager.CreateAsync(new IdentityRole(Helper.Patient));
+            }
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ApplicationUser user = new ApplicationUser
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    Name = model.Name
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(user);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.RoleName);
+                    await _signInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("Index", "Home");
+                }  
+            }
+            return View();
+        }
+    }
+}
